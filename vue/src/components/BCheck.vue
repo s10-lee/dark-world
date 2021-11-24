a2e6fd6a731fec241867cc4756a984d767f81bb9
s10 <quicksilverinveins@gmail.com>
2021-11-24 14:31:33 +0300

    checkbox component

---
+++ vue/src/components/BCheck.vue
@@ -0,0 +1,36 @@
+<template>
+  <label class="check">
+    <slot></slot>
+    <input type="checkbox"
+           @change="$emit('update:modelValue', $event.target.checked)"
+           :checked="modelValue">
+    <span class="mark"></span>
+  </label>
+</template>
+
+<script>
+import BLabel from './BLabel';
+export default {
+  name: 'BCheck',
+  components: { BLabel },
+  emits: ['update:modelValue'],
+  props: {
+    modelValue: String,
+    size: String
+  },
+  data() {
+    return {
+      sizes: ['xl', 'lg', 'md', 'sm', 'xl'],
+    }
+  },
+  computed: {
+    inputCss() {
+      const styles = ['form-control']
+      if (this.sizes.includes(this.size)) {
+        styles.push('form-control-' + this.size)
+      }
+      return styles
+    },
+  }
+}
+</script>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,17 +1,42 @@
 <template>
-    <div class="fixed-container">
-      <div class="container">
-        <div class="row mt-6">
-          <div class="col">
-
+  <div class="fixed-container">
+    <div class="container">
+      <div class="row mt-6 mb-3">
+        <div class="col">
+          <div class="mb-2">
+            <b-input label="Label" v-model="inputValue" placeholder="Text..."/>
+            value: {{ inputValue }}
           </div>
+          <div>
+            <b-check v-model="checkValue">Custom checkbox</b-check>
+            <br/>
+            value: {{ checkValue }}
+          </div>
+        </div>
+        <div class="col">
+          <b-text-area label="Label" v-model="textAreaValue"/>
+          value: {{ textAreaValue }}
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+        </div>
+        <div class="col">
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      inputValue: null,
+      textAreaValue: null,
+      checkValue: null,
+    }
+  }
 }
 </script>
