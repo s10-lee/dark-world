2dada1a617a841a2f628d94cf7b88f506c89935d
s10 <quicksilverinveins@gmail.com>
2022-02-15 00:30:38 +0300

    grabber fix

--- app/src/grab/routers.py
+++ app/src/grab/routers.py
@@ -17,11 +17,13 @@ async def grab_media_from_url(data: GrabUrl, user_id: uuid.UUID = Depends(get_cu
     item_id = uuid.uuid4()
     url = data.url
 
+    url_host = url.host
+
     # TODO: Grab service
-    if url.host == 'www.pinterest.com':
+    if url_host == 'www.pinterest.com':
         url = await grab_from_pinterest(url)
 
-    if url.host == 'dribbble.com':
+    if url_host == 'dribbble.com':
         url = await grab_from_dribble(url)
 
     extension = url.split('.')[-1]
@@ -30,7 +32,7 @@ async def grab_media_from_url(data: GrabUrl, user_id: uuid.UUID = Depends(get_cu
     if response.status == 200 and url:
         await save_file_media(response.data, path=user_id, filename=item_id, extension=extension)
         return await PinReceive.from_tortoise_orm(
-            await Pin.create(id=item_id, user_id=user_id, name=url.host, extension=extension)
+            await Pin.create(id=item_id, user_id=user_id, extension=extension, content_type=response.content_type)
         )
 
     return {'status': response.status}
